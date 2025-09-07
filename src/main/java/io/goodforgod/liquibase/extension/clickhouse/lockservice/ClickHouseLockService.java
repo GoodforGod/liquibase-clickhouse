69dca7e8069a1084b39747f3351fd4071d1a385c
Anton Kurako <goodforgod.dev@gmail.com>
2025-09-07 15:30:39 +0300

    ClickHouseLockService#releaseLock custom logic due to incorrectly working ClickHouse driver V2

--- src/main/java/io/goodforgod/liquibase/extension/clickhouse/lockservice/ClickHouseLockService.java
+++ src/main/java/io/goodforgod/liquibase/extension/clickhouse/lockservice/ClickHouseLockService.java
@@ -2,18 +2,26 @@ package io.goodforgod.liquibase.extension.clickhouse.lockservice;
 
 import io.goodforgod.liquibase.extension.clickhouse.database.ClickHouseDatabase;
 import io.goodforgod.liquibase.extension.clickhouse.statement.ClickhouseLockDatabaseChangeLogStatement;
+import java.util.logging.Level;
 import liquibase.Scope;
 import liquibase.changelog.ChangeLogHistoryServiceFactory;
 import liquibase.database.Database;
+import liquibase.database.ObjectQuotingStrategy;
 import liquibase.exception.DatabaseException;
 import liquibase.exception.LiquibaseException;
 import liquibase.exception.LockException;
 import liquibase.exception.UnexpectedLiquibaseException;
 import liquibase.executor.Executor;
 import liquibase.executor.ExecutorService;
+import liquibase.executor.jvm.ChangelogJdbcMdcListener;
 import liquibase.lockservice.StandardLockService;
 import liquibase.logging.Logger;
+import liquibase.logging.mdc.MdcKey;
+import liquibase.logging.mdc.MdcObject;
+import liquibase.logging.mdc.MdcValue;
+import liquibase.statement.SqlStatement;
 import liquibase.statement.core.RawParameterizedSqlStatement;
+import liquibase.statement.core.UnlockDatabaseChangeLogStatement;
 
 public class ClickHouseLockService extends StandardLockService {
 
@@ -125,6 +133,78 @@ public class ClickHouseLockService extends StandardLockService {
             try {
                 database.rollback();
             } catch (DatabaseException ignored) {}
+        }
+    }
+
+    // copy cat with fix for clickhouse driver v2 doesn't properly return
+    // executeUpdate/executeLargeUpdate updated rows in metadata
+    @Override
+    public void releaseLock() throws LockException {
+        ObjectQuotingStrategy incomingQuotingStrategy = null;
+        if (this.quotingStrategy != null) {
+            incomingQuotingStrategy = database.getObjectQuotingStrategy();
+            database.setObjectQuotingStrategy(this.quotingStrategy);
+        }
+
+        boolean success = false;
+        Executor executor = Scope.getCurrentScope().getSingleton(ExecutorService.class).getExecutor("jdbc", database);
+        try {
+            if (this.isDatabaseChangeLogLockTableCreated()) {
+                executor.comment("Release Database Lock");
+                database.rollback();
+                SqlStatement unlockStatement = new UnlockDatabaseChangeLogStatement();
+                int updatedRows = ChangelogJdbcMdcListener.query(database, ex -> ex.update(unlockStatement));
+                if (updatedRows != 1) {
+                    // 0 unlocked, 1 locked
+                    Integer lockedStatus = executor.queryForObject(
+                            new RawParameterizedSqlStatement(String.format("SELECT LOCKED FROM %s.%s",
+                                    database.getDefaultSchemaName(), database.getDatabaseChangeLogLockTableName())),
+                            Integer.class);
+
+                    if (!Integer.valueOf(0).equals(lockedStatus)) {
+                        SqlStatement countStatement = new RawParameterizedSqlStatement(
+                                String.format("SELECT COUNT(*) FROM %s",
+                                        database.getDatabaseChangeLogLockTableName()));
+
+                        throw new LockException(
+                                "Did not update change log lock correctly.\n\n" +
+                                        updatedRows +
+                                        " rows were updated instead of the expected 1 row using executor "
+                                        + executor.getClass().getName() + "" +
+                                        " there are "
+                                        + ChangelogJdbcMdcListener.query(database, ex -> ex.queryForList(countStatement)) +
+                                        " rows in the table");
+                    }
+                }
+                database.commit();
+                success = true;
+            }
+        } catch (Exception e) {
+            throw new LockException(e);
+        } finally {
+            try {
+                hasChangeLogLock = false;
+
+                database.setCanCacheLiquibaseTableInfo(false);
+                try (MdcObject releaseLocksOutcome = Scope.getCurrentScope().addMdcValue(MdcKey.RELEASE_LOCKS_OUTCOME, success
+                        ? MdcValue.COMMAND_SUCCESSFUL
+                        : MdcValue.COMMAND_FAILED)) {
+                    Scope.getCurrentScope().getLog(getClass()).log(success
+                            ? Level.INFO
+                            : Level.WARNING,
+                            (success
+                                    ? "Successfully released"
+                                    : "Failed to release") + " change log lock",
+                            null);
+                }
+
+                database.rollback();
+            } catch (DatabaseException e) {
+                Scope.getCurrentScope().getLog(getClass()).warning("Failed to rollback", e);
+            }
+            if (incomingQuotingStrategy != null) {
+                database.setObjectQuotingStrategy(incomingQuotingStrategy);
+            }
         }
     }
 
